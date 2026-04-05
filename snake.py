07a8717857461d84d5ea489536b0632d9aec8337
ugbajaivan <ugbajaivan@gmail.com>
2026-04-05 15:54:34 -0400

    Apple Added

--- snake.py
+++ snake.py
@@ -45,12 +45,17 @@ window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 # Initialize Game
 snake = Tile(5*TILE_SIZE,5*TILE_SIZE) # Single tile for snake's head
+food = Tile(10*TILE_SIZE,10*TILE_SIZE)
+
 
 def draw():
     global snake # Referencing external snake variable and not a local one in function
     
     # Draw Snake
     canvas.create_rectangle(snake.x, snake.y, (snake.x + TILE_SIZE), (snake.y + TILE_SIZE), fill= "lime green")
+    
+    # Draw Food
+    canvas.create_rectangle(food.x, food.y, (food.x + TILE_SIZE), (food.y + TILE_SIZE), fill= "red")
     
     window.after(100, draw) # Every 100ms, snake will be drawn. AKA Running at 10 fps
     
