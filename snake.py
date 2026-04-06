5ce8b723f0d6d5358f32d6158883eb656bc29804
ugbajaivan <ugbajaivan@gmail.com>
2026-04-06 00:14:53 -0400

    Snake Movement

--- snake.py
+++ snake.py
@@ -47,9 +47,42 @@ window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 snake = Tile(5*TILE_SIZE,5*TILE_SIZE) # Single tile for snake's head
 food = Tile(10*TILE_SIZE,10*TILE_SIZE)
 
+# Variables for Key listener
+velocity_x=0
+velocity_y=0
+
+
+def change_direction(e): # e is event
+    #print(e)
+    #print(e.keysym)
+    global velocity_x, velocity_y
+    
+    if e.keysym == "Up" and velocity_y != 1:
+        velocity_x = 0
+        velocity_y = -1
+    if e.keysym == "Down" and velocity_y != -1:
+        velocity_x = 0
+        velocity_y = 1
+    if e.keysym == "Left" and velocity_x != 1:
+        velocity_x = -1
+        velocity_y = 0
+    if e.keysym == "Right" and velocity_x != -1:
+        velocity_x = 1
+        velocity_y = 0
+
+
+def move():
+    global snake
+    
+    snake.x += velocity_x*TILE_SIZE
+    snake.y += velocity_y*TILE_SIZE
+
 
 def draw():
     global snake # Referencing external snake variable and not a local one in function
+    move()
+    
+    canvas.delete("all")
     
     # Draw Snake
     canvas.create_rectangle(snake.x, snake.y, (snake.x + TILE_SIZE), (snake.y + TILE_SIZE), fill= "lime green")
@@ -60,5 +93,5 @@ def draw():
     window.after(100, draw) # Every 100ms, snake will be drawn. AKA Running at 10 fps
     
 draw()
-
+window.bind("<KeyRelease>", change_direction)
 window.mainloop()
